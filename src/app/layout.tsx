e8d76500f575fb76383893a036a43c2e4a59a88b
BGMCandy <nielsbonde992@gmail.com>
2025-08-28 15:09:39 +0200

    ower

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./index/components/header";
 import Footer from "./index/components/footer";
-import Overlay from "./index/components/Overlay";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,6 @@ export default function RootLayout({
         style={{ margin: 0, padding: 0 }}
       > 
         <Header />
-        <Overlay />
         <main className="relative min-h-screen">
           {children}
         </main>
